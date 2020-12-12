068c0675c70cec55b530c4365297bd16767eb902
rubiyadav18 <73836805+rubiyadav18@users.noreply.github.com>
2020-12-11 22:24:59 -0800

    Add files via upload

---
+++ diffcult_paytern_ques_.py
@@ -0,0 +1,32 @@
+i=1
+while i<=5:
+    j=5
+    while j>=1:
+        if i==1:
+            print(j,end=" ")
+        if i==2:
+            print(j+5,end=" ")
+        if i==3:
+            print(j+10,end=" ")
+            
+        if i==4:
+            print(j+15,end=" ")
+        if i==5:
+            print(j+20,end=" ")
+        j=j-1
+    print()
+    i=i+1
+i=1
+while i<=3:
+    j=3
+    while j>=3:
+        if i>=3:
+            print(j,end=" ")
+        if i==2:
+            print(j+,end=" ")
+        if i==3:
+            print(j+2,end="  ")
+        j=j-1
+    print()
+    i=i+1
+        
